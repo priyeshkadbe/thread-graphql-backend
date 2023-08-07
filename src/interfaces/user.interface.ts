a82511a4627c7bde9b3a816af38fc753a7380c44
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-08-08 01:39:52 +0530

    removed salt from the db

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -15,3 +15,8 @@ export interface updateUserPayload {
   email: string;
   password: string;
 }
+
+export interface userSignInPayload{
+  email:string;
+  password:string;
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,6 +6,7 @@ import {
   createUserPayload,
   UserId,
   updateUserPayload,
+  userSignInPayload
 } from '../interfaces/user.interface';
 
 import {hashWithCustomSalt} from "../utils/HashAndSaltGenerator"
@@ -21,14 +22,18 @@ class UserService {
     const { firstName, lastName, profileImageUrl, email, password } = payload;
     console.log('paylaod received',payload)
    try {
-      const {hashedPassword,salt} = await hashWithCustomSalt(password)
+      
+     const userExits= await this.userRepository.getUserByEmail(email)
+     if(userExits){
+       return "email already taken";
+     }
+     
       const user = await this.userRepository.create({
         firstName,
         lastName,
         profileImageUrl,
-        password: hashedPassword,
-        email,
-        salt,
+        password ,
+        email, 
       });
       
       return user;
@@ -36,6 +41,16 @@ class UserService {
       console.log('something went wrong in the service layer ', error);
       throw { error };
     }
+  }
+
+  async signIn(payload:userSignInPayload){
+   try {
+         
+   } catch (error) {
+       console.log('something went wrong in the service layer ', error);
+       throw { error };
+   
+   } 
   }
 }
 
